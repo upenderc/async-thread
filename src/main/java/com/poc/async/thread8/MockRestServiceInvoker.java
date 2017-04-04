222864544d7d93bf830929cd90a33d9303f613fa
upenderc <upenderc@gmail.com>
2017-04-04 13:44:35 -0500

    commented approach2

--- src/main/java/com/poc/async/thread8/MockRestServiceInvoker.java
+++ src/main/java/com/poc/async/thread8/MockRestServiceInvoker.java
@@ -27,10 +27,15 @@ public class MockRestServiceInvoker {
 	 ResultHandler resultHandler = new ResultHandlerImpl();
 	  
 	 //submitting task async
-				CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(1),mroPredicate), executorService).thenAccept(resultHandler::collect);
+			/*	CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(1),mroPredicate), executorService).thenAccept(resultHandler::collect);
 				CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(2),mroPredicate), executorService).thenAccept(resultHandler::collect);
 				CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(3),mroPredicate), executorService).thenAccept(resultHandler::collect);
 				CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(4),mroPredicate), executorService).thenAccept(resultHandler::collect);
+				*/
+	 CompletableFuture<List<String>> cf1=CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(1),mroPredicate), executorService);
+	 CompletableFuture<List<String>> cf2=CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(2),mroPredicate), executorService);
+	 CompletableFuture<List<String>> cf3=CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(3),mroPredicate), executorService);
+	 CompletableFuture<List<String>> cf4=CompletableFuture.supplyAsync(()->filter.apply(retriever.pageRetriever(4),mroPredicate), executorService);
 		//Approach-1
 		List<String>  finalResults = cf1.thenCombine(cf2, (a,b)->{a.addAll(b);return a;}).thenCombine(cf3, (a,b)->{a.addAll(b);return a;}).thenCombine(cf4, (a,b)->{a.addAll(b);return a;}).join();
 		System.out.println(finalResults); 
